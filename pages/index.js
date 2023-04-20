76d7d1e8078b48f1b779ab5dd562f6ac7ac7fce3
Benatus <130522682+Benatus@users.noreply.github.com>
2023-04-20 19:29:03 +0000

    alterando mensagem do site para aviso

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Ambiente para subir o Site, agora vai Gabriel</h1>
+    return <h1>Olá ainda estamos trabalhando no desenvolvimento do site, tenha um bom dia e beba água.</h1>
 }
 
 export default Home;
