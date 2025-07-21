7bdf3e485c7172121f29ffd8c74e7eb04e33c867
Benatus <igorbenato@hotmail.com>
2025-07-21 13:39:32 -0300

    refactor: reorganiza a estrutura dos testes automatizados

--- testes/integrations/api/v1/status/get.test.js
+++ testes/integrations/api/v1/status/get.test.js
@@ -3,22 +3,27 @@ import orquestrator from "testes/orquestrator.js";
 beforeAll(async () => {
   await orquestrator.waitForAllServices();
 });
-test("GET to api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
 
-  const result = await response.json();
-  expect(response.status).toBe(200);
-  expect(result.update_at).toBeDefined();
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retorna o status da aplicação", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-  expect(result).toEqual({
-    update_at: expect.any(String),
-    dependencies: {
-      database: {
-        db_version: "16.0",
-        db_max_connections: 100,
-        db_active_connections: 1,
-      },
-    },
+      const result = await response.json();
+      expect(response.status).toBe(200);
+      expect(result.update_at).toBeDefined();
+
+      expect(result).toEqual({
+        update_at: expect.any(String),
+        dependencies: {
+          database: {
+            db_version: "16.0",
+            db_max_connections: 100,
+            db_active_connections: 1,
+          },
+        },
+      });
+      console.log(result);
+    });
   });
-  console.log(result);
 });
