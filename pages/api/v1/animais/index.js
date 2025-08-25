0b44ad478a56a8c3664d0f5d559f5b954cc7d9d9
Benatus <igorbenato@hotmail.com>
2025-08-25 00:48:11 -0300

    feat: adiciona funcionalidade de uplodad de imagem (para o Supabase)

--- pages/api/v1/animais/index.js
+++ pages/api/v1/animais/index.js
@@ -13,15 +13,12 @@ async function animais(req, res) {
       console.log("Entrou no PUT da API animal");
       const data = await UpdateAnimal(req.body);
       res.status(200).json({ success: true, data: data });
+    } else if (req.method === "POST") {
+      const data = await CreateAnimal(req.body);
+
+      res.status(200).json({ success: true, data: data });
     } else {
       res.status(405).json({ error: "Método não permitido" });
-    }
-    if (req.method === "POST") {
-      res.status(200).json({ success: true, data: [] });
-    }
-    if (req.method === "PUT") {
-      const data = await UpdateAnimal(req.body);
-      res.status(200).json({ success: true, data: data });
     }
   } catch (err) {
     res.status(500).json({ error: "Erro inesperado: " + err.message });
@@ -50,14 +47,15 @@ async function UpdateAnimal(animal) {
       text: `SELECT id FROM raca WHERE LOWER(nome_raca) = LOWER($1) AND especie_id = $2`,
       values: [animal.raca, especie_id], // garante que a raça pertence à espécie correta
     });
-
+    let raca_id = null;
     if (racaResult.rows.length === 0) {
-      throw new Error(
+      console.log(
         `Raça "${animal.raca}" não encontrada para a espécie "${animal.especie}"`,
       );
+    } else {
+      raca_id = racaResult.rows[0].id;
     }
 
-    const raca_id = racaResult.rows[0].id;
     console.log("ID da raça encontrada:", raca_id);
     console.log("Atualizando animal com ID:", animal.id);
     // 3️⃣ Atualizar o animal
@@ -124,4 +122,30 @@ async function getLastUpdatedAnimals() {
   }
 }
 
+async function CreateAnimal(animal) {
+  animal = animal.body;
+  try {
+    const queryObject = {
+      text: `
+        INSERT INTO animal (nome, idade, status, sexo, especie_id, raca_id, imagem_url)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *;
+      `,
+      values: [
+        animal.nome || null,
+        animal.idade,
+        animal.status || null,
+        animal.sexo,
+        animal.especie_id,
+        animal.raca_id || null,
+        animal.imagem_url || null,
+      ],
+    };
+    const result = await database.query(queryObject);
+    return result.rows[0];
+  } catch (err) {
+    console.error("Error creating animal:", err);
+    throw new Error("Failed to create animal");
+  }
+}
 export default animais;
