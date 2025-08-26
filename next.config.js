3989abdca87491e930f6e2498ac29550b0e7191a
Benatus <igorbenato@hotmail.com>
2025-08-26 01:13:51 -0300

    fix: corrige chamada variavel de ambiente

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-const domain = process.env.SUPABASE_URL.replace("https://", "").replace("/");
 module.exports = {
   images: {
-    domains: [domain],
+    domains: [process.env.SUPABASE_URL],
   },
 };
