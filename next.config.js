b02b7fc6deea76788210a11ed167ca61f426b6cd
Benatus <igorbenato@hotmail.com>
2025-08-26 01:10:48 -0300

    fix: corrige erro no next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
+const domain = process.env.SUPABASE_URL.replace("https://", "").replace("/");
 module.exports = {
   images: {
-    domains: [process.env.SUPABASE_URL.replace("https://", "").replace("/")],
+    domains: [domain],
   },
 };
